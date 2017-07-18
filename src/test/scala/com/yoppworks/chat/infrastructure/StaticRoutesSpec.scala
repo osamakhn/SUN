82157e731a2315187cb598be270bccf7667f3f1c
James Townley <jtownley@yoppworks.com>
2017-07-18 09:27:20 -0600

    Adding additional class headers

---
+++ src/test/scala/com/yoppworks/chat/infrastructure/StaticRoutesSpec.scala
@@ -0,0 +1,8 @@
+package com.yoppworks.chat.infrastructure
+
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import org.scalatest.{ FreeSpec, Matchers }
+
+class StaticRoutesSpec extends FreeSpec with Matchers with ScalatestRouteTest with StaticRoutes {
+
+}
