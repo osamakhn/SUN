14e3d3b4c0dc56c13dae4a6725ee5e2847e4123f
Osama Khan <mail@osamakhan.com>
2017-07-22 14:51:32 -0700

    source, sink, flow end

--- src/main/scala/com/yoppworks/chat/infrastructure/ChatRoutes.scala
+++ src/main/scala/com/yoppworks/chat/infrastructure/ChatRoutes.scala
@@ -1,21 +1,37 @@
 package com.yoppworks.chat.infrastructure
 
-import akka.http.scaladsl.common.{ EntityStreamingSupport, JsonEntityStreamingSupport }
+import akka.http.scaladsl.common.EntityStreamingSupport
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
-
-import com.yoppworks.chat.api.Chat
+import akka.stream.scaladsl.{Flow, Source}
 import com.yoppworks.chat.domain.ChatMessage
 import spray.json.DefaultJsonProtocol
 
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
+  // mapping; simple; can be fancy
+  implicit val chatMessageFormat = jsonFormat3(ChatMessage.apply)
+  // the coplete won't work so we need json-streaming support
+  implicit val jsonEntityStreamingSupport = EntityStreamingSupport.json()
 }
 
 
 trait ChatRoutes extends JsonSupport {
-//  val api : Chat
-  val chatRoutes: Route = ???
+  //  val api : Chat
+  // this is gonna stream message via kafka stream
+
+  val chatRoutes: Route = pathPrefix("api") {
+    path("chat") {
+      get {
+        val source: Source[Int, Any] = Source(Range(0, 200000))
+        val flow: Flow[Int, ChatMessage, Any] = Flow[Int].map(num => {
+          Thread.sleep(1000)
+          ChatMessage("System", s"Message: $num", "Main")
+        })
+        complete(source.via(flow))
+      }
+    }
+  }
 }
 
 
--- src/main/scala/com/yoppworks/chat/infrastructure/Main.scala
+++ src/main/scala/com/yoppworks/chat/infrastructure/Main.scala
@@ -3,16 +3,17 @@ package com.yoppworks.chat.infrastructure
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
 
 
-object Main extends App with StaticRoutes{
+object Main extends App with StaticRoutes with ChatRoutes{
   //  val api : Chat = ???
 
   implicit val system = ActorSystem("SafetyDance")
   implicit val materializer = ActorMaterializer()
 
-  val route: Route = staticRoutes
+  val route: Route = staticRoutes ~ chatRoutes
 
   val bindingFuture = Http().bindAndHandle(route, "localhost", 8081)
 }
